d382679e4d750e82ff74f505c0b174ee3e94a3c3
chenli <chenli@sucsoft.com>
2022-05-11 21:23:44 +0800

    feat: migrate from https://github.com/Freedomisgood/When_Coding_in_ZJU

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# leetcode-daily-push
-Leetcode每日一题推送, GithubAction
+# Leetcode每日一题推送
+> 舍友都是ACM选手, 所以每天都有一起刷题的需求, 那就做下Leetcode的每日一题好咯。
+> 
+> 个人使用: 由DingBot推送到舍友的钉钉群中
+
+## 推送模块:
+
+结合使用推送模块, 修改`pusher.py`内的配置内容，可以实现如下推送
+- ★钉钉群机器人
+  - DING_ACCESS_TOKEN、DING_SECRET
+- pushplus
+  - PUSHPLUS_TOKEN
+- serverchan
+  - SERVERCHAN_TOKEN
+
+注：需要设置环境变量(如果是GithubAction中使用的话，则是设置secrets)or修改pusher中的配置代码.
+
+## 更新日志:
+
+- 2022年5月10日: 丰富推送模块, 并提供GithubAction secrets修改
+  在仓库的secrets中设置
+    - PUSHPLUS_TOKEN
+    - SERVERCHAN_TOKEN
+    - DING_ACCESS_TOKEN、DING_SECRET
+- 2022年3月1日: 编写脚本
+
+推荐使用钉钉群机器人，效果如下:
+
+![](./pics/demo.jpg)
+
+## Github Action设置:
+
+> 没有服务器的朋友, 通过Fork、设置secrets后, 触发Github Action的workflow就每天都能收到推送啦。
+
+![](./pics/action_secret.jpg)
---
+++ leetcode_runner.py
@@ -0,0 +1,52 @@
+import json
+
+import requests
+
+
+def getQuestion():
+    base_url = 'https://leetcode-cn.com'
+
+    # 获取今日每日一题的题名(英文)
+    response = requests.post(base_url + "/graphql", json={
+        "operationName": "questionOfToday",
+        "variables": {},
+        "query": "query questionOfToday { todayRecord {   question {     questionFrontendId     questionTitleSlug     __typename   }   lastSubmission {     id     __typename   }   date   userStatus   __typename }}"
+    })
+    leetcodeTitle = json.loads(response.text).get('data').get('todayRecord')[0].get("question").get('questionTitleSlug')
+
+    # 获取今日每日一题的所有信息
+    response = requests.post(base_url + "/graphql",
+                             json={"operationName": "questionData", "variables": {"titleSlug": leetcodeTitle},
+                                   "query": "query questionData($titleSlug: String!) {  question(titleSlug: $titleSlug) {    questionId    questionFrontendId    boundTopicId    title    titleSlug    content    translatedTitle    translatedContent    isPaidOnly    difficulty    likes    dislikes    isLiked    similarQuestions    contributors {      username      profileUrl      avatarUrl      __typename    }    langToValidPlayground    topicTags {      name      slug      translatedName      __typename    }    companyTagStats    codeSnippets {      lang      langSlug      code      __typename    }    stats    hints    solution {      id      canSeeDetail      __typename    }    status    sampleTestCase    metaData    judgerAvailable    judgeType    mysqlSchemas    enableRunCode    envInfo    book {      id      bookName      pressName      source      shortDescription      fullDescription      bookImgUrl      pressImgUrl      productUrl      __typename    }    isSubscribed    isDailyQuestion    dailyRecordStatus    editorType    ugcQuestionId    style    __typename  }}"})
+    jsonText = json.loads(response.text).get('data').get("question")
+    # 题目题号
+    no = jsonText.get('questionFrontendId')
+    # 题名（中文）
+    leetcodeTitle = jsonText.get('translatedTitle')
+    # 题目难度级别
+    level = jsonText.get('difficulty')
+    # 题目内容
+    context = jsonText.get('translatedContent')
+    url = "https://leetcode-cn.com/problems/" + "-".join(jsonText.get("title").split())
+    return no, level, leetcodeTitle, context, url
+
+
+def run():
+    """获取每日一题, 并推送"""
+    no, level, leetcodeTitle, context, url = getQuestion()
+    try:
+        p = getattr(__import__("pusher"), "p")
+    except AttributeError:
+        # 未配置pusher
+        print("未启用pusher, 请在pusher.py中打开注释启用, 并配置使用")
+        # post(no, level, leetcodeTitle, context, url)
+    else:
+        print("采用Pusher")
+        print(p.push(title="Leetcode: {}-{}-{}".format(no, level, leetcodeTitle),
+                     content="Urls: {}\n\n文本内容: \n\n{}".format(url, context),
+                     url=url
+                     ))
+
+
+if __name__ == '__main__':
+    run()
